b9dd62eeaf772e49e6bfb1d012d828d8f332ed54
Joyel <Varghesejoyel71@gmail.com>
2025-01-02 22:35:55 +0530

    api configuration return added in 403

--- client/src/infrastructure/api/api.ts
+++ client/src/infrastructure/api/api.ts
@@ -22,8 +22,10 @@ api.interceptors.response.use(
     async (error) => {
         const originalRequest = error.config;
         if(error.response.status === 403 ){
-            window.location.href = '/';
-            return;
+            const message = error.response?.data?.message || "Forbidden";
+            localStorage.clear();
+            alert(message); 
+            window.location.href = '/login';
         }
         if (error.response?.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true; 
