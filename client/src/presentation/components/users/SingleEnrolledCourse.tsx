30ed10a025c06533a9d5241a43d1ff5d9a415f17
Joyel <Varghesejoyel71@gmail.com>
2025-01-14 10:40:09 +0530

    improvement added on UI part twp

--- client/src/presentation/components/users/SingleEnrolledCourse.tsx
+++ client/src/presentation/components/users/SingleEnrolledCourse.tsx
@@ -103,7 +103,13 @@ const CoursePage = () => {
     setSelectedOptions((prev) => ({ ...prev, [questionId]: option }));
   };
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+      </div>
+    );
+  }
 
   if (error) return <div>Error: {error}</div>;
 
