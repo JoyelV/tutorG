51d85b4125307081690aeec24d46a0a455569bcd
Joyel <Varghesejoyel71@gmail.com>
2025-01-28 12:25:31 +0530

    final

--- client/src/infrastructure/api/api.ts
+++ client/src/infrastructure/api/api.ts
@@ -27,7 +27,7 @@ api.interceptors.response.use(
             alert(message); 
             window.location.href = '/login';
         }
-        if (error.response?.status === 403 && !originalRequest._retry) {
+        if (error.response?.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true; 
             try {
                 const newToken = await refreshAccessToken();
